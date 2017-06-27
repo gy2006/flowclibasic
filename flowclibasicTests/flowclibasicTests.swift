6eb517527afe0dc1f9753fef36a51421b682499b
Yang Guo <gy@fir.im>
2017-06-27 18:36:55 +0800

    set to pass tet

--- flowclibasicTests/flowclibasicTests.swift
+++ flowclibasicTests/flowclibasicTests.swift
@@ -29,7 +29,7 @@ class flowclibasicTests: XCTestCase {
     }
     
     func testShouldFail() {
-        XCTAssertEqual("hello", "1")
+        XCTAssertEqual("1", "1")
     }
     
     func testPerformanceExample() {
